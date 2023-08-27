2044de78b39f4b5c10a367b1a3fbe5cf1befb7bb
叶 万晟 <kano_mansei@gcrest.com>
2023-08-28 01:14:57 +0900

    サーバーサイドのリソース解放周りを修正

--- Server.java
+++ Server.java
@@ -18,26 +18,37 @@ class Worker extends Thread {
 
     public void run() {
         try (InputStream in = socket.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                PrintWriter tempWriter = new PrintWriter(socket.getOutputStream(), true);
+                Socket tempSocket = socket) {
 
-            String line;
-            writer = new PrintWriter(socket.getOutputStream(), true);
-            synchronized (writer) {
+            writer = tempWriter;
+            synchronized (Server.writers) {
                 Server.writers.add(writer);
             }
 
             // クライアントからの投稿を待ち受ける
+            String line;
             while ((line = reader.readLine()) != null) {
+
+                // 接続している全てのクライアントにブロードキャスト
                 synchronized (Server.writers) {
-                    for (PrintWriter writer : Server.writers) {
-                        writer.println(line);
+                    for (PrintWriter pw : Server.writers) {
+                        pw.println(line);
                     }
                 }
+
+                // 受け取ったメッセージはサーバー側でも表示
                 System.out.println(line);
             }
 
         } catch (Exception e) {
             System.out.println(e);
+        } finally {
+            // サーバーが持つprintwriter一覧から自身のprintwriterを削除
+            synchronized (Server.writers) {
+                Server.writers.remove(writer);
+            }
         }
     }
 }
